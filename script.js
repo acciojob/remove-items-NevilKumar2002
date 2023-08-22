47daef89fd57c50531bd10d3ec6e05f30a7633e7
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-22 12:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// const color=documnet.getElementById("colorSelect"
+function removeFunction()
+	{
+		var element=document.getElementById("colorSelect");
+		element.remove(element.selectedIndex)
+	}
